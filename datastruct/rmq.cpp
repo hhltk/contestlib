31efb6e2af5798d0950c909bfa1c3c88214c1eef
hltk <hltk@hltk.fi>
2020-03-23 23:52:56 +0200

    added rmq

---
+++ datastruct/rmq.cpp
@@ -0,0 +1,18 @@
+struct RMQ {
+	int lvl(int x) { return 31 - __builtin_clz(x); }
+	vector<vector<int>> spr;
+	RMQ() {}
+	RMQ(vector<int> &v) {
+		spr.push_back(v);
+		for (int j = 1; (1 << j) <= (int) v.size(); ++j) {
+			spr.push_back(vector<int>(v.size() - (1 << j) + 1));
+			for (int i = 0; i < (int) spr[j].size(); ++i) {
+				spr[j][i] = min(spr[j - 1][i], spr[j - 1][i + (1 << (j - 1))]);
+			}
+		}
+	}
+	int get(int l, int r) {
+		int d = lvl(r - l + 1);
+		return min(spr[d][l], spr[d][r - (1 << d) + 1]);
+	}
+};
