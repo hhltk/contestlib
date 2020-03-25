c162b3ed029254fe48685c13f16b4ff58d7bf7d2
hltk <hltk@hltk.fi>
2020-03-25 23:19:44 +0200

    rmq improvements

--- datastruct/rmq.cpp
+++ datastruct/rmq.cpp
@@ -1,15 +1,18 @@
+#define sz(x) (int) x.size()
+#define all(x) begin(x), end(x)
+using vi = vector<int>;
+
 template <typename T> struct RMQ {
-	vector<vector<int>> spr;
+	vector<vi> spr;
 	vector<T> v;
 	int lvl(int x) { return 31 - __builtin_clz(x); }
 	int combine(int a, int b) { return v[a] <= v[b] ? a : b; }
 	RMQ() {}
 	RMQ(vector<T> &v_) : v(v_) {
-		spr = {vector<int>(v.size())};
-		iota(spr[0].begin(), spr[0].end(), 0);
-		for (int j = 1; (1 << j) <= (int) v.size(); ++j) {
-			spr.push_back(vector<int>(v.size() - (1 << j) + 1));
-			for (int i = 0; i < (int) spr[j].size(); ++i) {
+		spr = {vi(sz(v))}; iota(all(spr[0]), 0);
+		for (int j = 1; (1 << j) <= sz(v); ++j) {
+			spr.push_back(vi(sz(v) - (1 << j) + 1));
+			for (int i = 0; i < sz(spr[j]); ++i) {
 				spr[j][i] = combine(spr[j - 1][i], spr[j - 1][i + (1 << (j - 1))]);
 			}
 		}
