d512b2b2510e1a3324d22eeb3d4c1453b85a9393
hltk <hltk@hltk.fi>
2020-04-16 11:27:21 +0300

    modint modifications

--- math/modint.cpp
+++ math/modint.cpp
@@ -1,11 +1,8 @@
 template <typename T> T pow(T a, ll b) { T r = 1; for ( ; b; b /= 2, a *= a) if (b % 2) r *= a; return r; }
 
 // source: ecnerwala, own, benq
-template <int MOD>
-struct modint {
-private:
+template <int MOD> struct modint {
 	int v;
-public:
 	modint() : v(0) {}
 	modint(ll x) : v(x % MOD) { if (v < 0) v += MOD; }
 	explicit operator int() const { return v; }
@@ -13,9 +10,6 @@ public:
 
 	friend ostream& operator<<(ostream& out, const modint& a) { return out << int(a); }
 	friend istream& operator>>(istream& in, modint& a) { ll x; in >> x; a = modint(x); return in; }
-
-	friend bool operator==(const modint& a, const modint& b) { return a.v == b.v; }
-	friend bool operator!=(const modint& a, const modint& b) { return a.v != b.v; }
 
 	modint inv() const { return pow(*this, MOD - 2); } // only works if MOD is prime
 	friend modint inv(const modint& x) { return x.inv(); }
