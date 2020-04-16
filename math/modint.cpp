a4e339ecd5a6187bd7fcebf5528b312fde570c37
hltk <hltk@hltk.fi>
2020-04-16 11:24:03 +0300

    shortened modint

--- math/modint.cpp
+++ math/modint.cpp
@@ -1,9 +1,6 @@
-template <typename T>
-T pow(T a, ll b) {
-	T r = 1; for ( ; b; b /= 2, a *= a) if (b % 2) r *= a; return r;
-}
+template <typename T> T pow(T a, ll b) { T r = 1; for ( ; b; b /= 2, a *= a) if (b % 2) r *= a; return r; }
 
-// source: ecnerwala
+// source: ecnerwala, own, benq
 template <int MOD>
 struct modint {
 private:
@@ -22,47 +19,16 @@ public:
 
 	modint inv() const { return pow(*this, MOD - 2); } // only works if MOD is prime
 	friend modint inv(const modint& x) { return x.inv(); }
-	modint neg() const { return modint(v ? MOD - v : 0); }
-	friend modint neg(const modint& x) { return x.neg(); }
 
-	modint operator-() const { return neg(); }
-	modint operator+() const { return modint(*this); }
+	modint &operator+=(const modint& a) { v += a.v; if (v >= MOD) v -= MOD; return *this; }
+	modint &operator-=(const modint& a) { v -= a.v; if (v < 0) v += MOD; return *this; }
+	modint &operator*=(const modint& a) { v = ll(v) * ll(a.v) % MOD; return *this; }
+	modint &operator/=(const modint& a) { return *this *= a.inv(); }
 
-	modint &operator++() {
-		v++;
-		if (v == MOD) v = 0;
-		return *this;
-	}
-	modint &operator--() {
-		v--;
-		if (v < 0) v += MOD;
-		return *this;
-	}
-
-	modint &operator+=(const modint& a) {
-		v += a.v;
-		if (v >= MOD) v -= MOD;
-		return *this;
-	}
-	modint &operator-=(const modint& a) {
-		v -= a.v;
-		if (v < 0) v += MOD;
-		return *this;
-	}
-	modint &operator*=(const modint& a) {
-		v = ll(v) * ll(a.v) % MOD;
-		return *this;
-	}
-	modint &operator/=(const modint& a) {
-		return *this *= a.inv();
-	}
-
-	friend modint operator++(const modint& a, int) { modint r = a; ++a; return r; }
-	friend modint operator--(const modint& a, int) { modint r = a; --a; return r; }
 	friend modint operator+(const modint& a, const modint& b) { return modint(a) += b; }
 	friend modint operator-(const modint& a, const modint& b) { return modint(a) -= b; }
 	friend modint operator*(const modint& a, const modint& b) { return modint(a) *= b; }
 	friend modint operator/(const modint& a, const modint& b) { return modint(a) /= b; }
 };
 
-using mint = modint<int(1e9)+7>;
+using mint = modint<MOD>;
