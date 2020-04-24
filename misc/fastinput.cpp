42c2ebfb3de18d853d0da181beb56348c6cca364
hltk <hltk@hltk.fi>
2020-04-24 11:36:32 +0300

    more formatted files

--- misc/fastinput.cpp
+++ misc/fastinput.cpp
@@ -14,8 +14,10 @@ struct Reader {
 int next_int() {
 	char c;
 	for (c = reader(); c != '-' && !isdigit(c); c = reader());
-	if (c == '-') return -next_int();
+	if (c == '-')
+		return -next_int();
 	int a = c - '0';
-	while (isdigit(c = reader())) a = a * 10 + c -'0';
+	while (isdigit(c = reader()))
+		a = a * 10 + c - '0';
 	return a;
 }
--- misc/rand.cpp
+++ misc/rand.cpp
@@ -1,7 +1,4 @@
 
-mt19937 rng((unsigned int) chrono::steady_clock::now().time_since_epoch().count());
+mt19937 rng((unsigned int)chrono::steady_clock::now().time_since_epoch().count());
 
-template<typename T>
-int rand() {
-	return uniform_int_distribution<T>()(rng);
-}
+template <typename T> int rand() { return uniform_int_distribution<T>()(rng); }
