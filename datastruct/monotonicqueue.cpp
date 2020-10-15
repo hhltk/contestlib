af55ffdb23128f43bb6334f73eb2514ee8dbe8d1
Henrik Aalto <hltk@hltk.fi>
2020-10-15 20:46:20 +0300

    update monotonicqueue.cpp

--- datastruct/monotonicqueue.cpp
+++ datastruct/monotonicqueue.cpp
@@ -1,26 +1,19 @@
-// source: benq, own
-template <typename T> struct minmaxdeque {
-    bool max_mode;
+template <class T, class Compare> struct MonotonicQueue {
     int l = 0, r = 0;
     deque<pair<T, int>> d;
 
-    minmaxdeque(bool max_mode_ = false) : max_mode(max_mode_) {
-    }
-
-    bool compare(T a, T b) {
-        return (max_mode && a <= b) || (!max_mode && b <= a);
-    }
     void push(T x) {
-        while (d.size() && compare(d.back().first, x))
+        while (!d.empty() && !Compare{}(d.back().first, x))
             d.pop_back();
         d.push_back({x, r++});
     }
+
     void pop() {
         if (d.front().second == l++)
             d.pop_front();
     }
+
     T get() {
-        return d.size() ? d.front().first
-                        : (max_mode ? 0 : numeric_limits<T>::max());
+        return d.front().first;
     }
 };
