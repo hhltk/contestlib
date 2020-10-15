d82f4f794f381a47f7e4e24b4594adc4e72fdd5f
Henrik Aalto <hltk@hltk.fi>
2020-10-15 18:59:57 +0300

    update rmq.cpp

--- datastruct/rmq.cpp
+++ datastruct/rmq.cpp
@@ -1,33 +1,25 @@
-#define sz(x) (int)x.size()
-#define all(x) begin(x), end(x)
-using vi = vector<int>;
-
-// source: own, benq
-template <typename T> struct RMQ {
-    vector<vi> spr;
+// source: own, Benq
+// O(n log n) preprocessing, O(1) queries
+template <class T, class Comparator = std::less<T>> struct RangeMinimumQuery {
+    vector<vector<int>> spr;
     vector<T> v;
 
-    RMQ(const vector<T> &v_) : v(v_) {
-        spr = {vi(sz(v))};
-        iota(all(spr[0]), 0);
-        for (int j = 1; (1 << j) <= sz(v); ++j) {
-            spr.push_back(vi(sz(v) - (1 << j) + 1));
-            for (int i = 0; i < sz(spr[j]); ++i) {
-                spr[j][i] =
-                    combine(spr[j - 1][i], spr[j - 1][i + (1 << (j - 1))]);
+    RangeMinimumQuery(const vector<T> &v) : v(v) {
+        spr = {vector<T>(v.size())};
+        iota(spr[0].begin(), spr[0].end(), 0);
+        for (int j = 1; (1 << j) <= int(v.size()); ++j) {
+            spr.push_back(vector<int>(int(v.size()) - (1 << j) + 1));
+            for (int i = 0; i < int(spr[j].size()); ++i) {
+                spr[j][i] = min(spr[j - 1][i], spr[j - 1][i + (1 << (j - 1))], Comparator{});
             }
         }
     }
 
-    int combine(int a, int b) {
-        return v[a] <= v[b] ? a : b;
-    }
-
-    int get(int l, int r) {
+    int argmin(int l, int r) {
         int d = 31 - __builtin_clz(r - l + 1);
-        return combine(spr[d][l], spr[d][r - (1 << d) + 1]);
+        return min(spr[d][l], spr[d][r - (1 << d) + 1], Comparator{});
     }
-    T getval(int l, int r) {
-        return v[get(l, r)];
+    int getmin(int l, int r) {
+        return v[argmin(l, r)];
     }
 };
