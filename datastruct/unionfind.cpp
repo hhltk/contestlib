60bb576cd8cf2f161d053dbfe6d513ed65a168e9
Henrik Aalto <hltk@hltk.fi>
2020-10-28 15:49:14 +0200

    clean up unionfind.cpp

--- datastruct/unionfind.cpp
+++ datastruct/unionfind.cpp
@@ -3,32 +3,23 @@ using namespace std;
 
 // source: kactl, own
 struct UnionFind {
-  int cmp_cnt;
   vector<int> p;
-  UnionFind(int n) : cmp_cnt(n), p(n, -1) {}
 
-  bool same_set(int a, int b) {
-    return find(a) == find(b);
+  UnionFind(int n) : p(n, -1) {}
+
+  inline int get(int s) {
+    return p[s] < 0 ? s : (p[s] = get(p[s]));
   }
 
-  int find(int s) {
-    return p[s] < 0 ? s : p[s] = find(p[s]);
-  }
-
-  int size(int s) {
-    return -p[find(s)];
-  }
-
-  bool unite(int a, int b) {
-    a = find(a);
-    b = find(b);
+  inline bool unite(int a, int b) {
+    a = get(a);
+    b = get(b);
     if (a != b) {
       if (p[b] < p[a]) {
         swap(a, b);
       }
       p[a] += p[b];
       p[b] = a;
-      cmp_cnt--;
     }
     return a != b;
   }
