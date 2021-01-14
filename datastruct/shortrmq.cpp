5ee9f20ecab2a6519208de0849a586df069f8ad0
Henrik Aalto <hltk@hltk.fi>
2021-01-14 15:57:37 +0200

    shortrmq.cpp: fix types

--- datastruct/shortrmq.cpp
+++ datastruct/shortrmq.cpp
@@ -9,17 +9,17 @@ using namespace std;
 // O(n log n) preprocessing, O(1) queries
 template <class T, class Comparator = less<T>>
 struct RMQ {
-	vector<vector<int>> spr;
+	vector<vector<T>> spr;
 	RMQ(const vector<T> &v) {
 		spr = {v};
 		for (int j = 1; (1 << j) <= sz(v); ++j) {
-			spr.push_back(vector<int>(sz(v) - (1 << j) + 1));
+			spr.push_back(vector<T>(sz(v) - (1 << j) + 1));
 			for (int i = 0; i < sz(spr[j]); ++i) {
 				spr[j][i] = min(spr[j - 1][i], spr[j - 1][i + (1 << (j - 1))], Comparator());
 			}
 		}
 	}
-	int getmin(int l, int r) {
+	T getmin(int l, int r) {
 		int d = __lg(r - l + 1);
 		return min(spr[d][l], spr[d][r - (1 << d) + 1], Comparator());
 	}
