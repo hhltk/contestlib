03daa6b0b1ce148e1a659db626865c52d5346a28
Henrik Aalto <hltk@hltk.fi>
2021-01-20 11:31:58 +0200

    add bins.cpp

---
+++ misc/bins.cpp
@@ -0,0 +1,10 @@
+// Finds the partition point of f in range [l, r)
+// i.e. minimum x such that f(x) = true and x \in [l, r)
+auto bins = [](auto l, auto r, auto f) {
+	while (l < r) {
+		int m = l + (r - l) / 2;
+		if (f(m)) r = m;
+		else l = m + 1;
+	}
+	return l;
+};
