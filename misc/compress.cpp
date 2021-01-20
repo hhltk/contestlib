5c97e068c260dbad2a4090b77b7fc6019de343dc
Henrik Aalto <hltk@hltk.fi>
2021-01-20 14:17:44 +0200

    compress.cpp

---
+++ misc/compress.cpp
@@ -0,0 +1,17 @@
+template <typename T>
+vector<int> compress(const vector<T>& orig) {
+	auto c = orig;
+	stable_sort(c.begin(), c.end());
+	auto bins = [&](T x) {
+		int l = 0, r = int(c.size());
+		while (l < r) {
+			int m = (l + r) / 2;
+			if (c[m] >= x) r = m;
+			else l = m + 1;
+		}
+		return l;
+	};
+	vector<int> ret(orig.size());
+	for (int i = 0; i < int(ret.size()); ++i) ret[i] = bins(orig[i]);
+	return ret;
+}
