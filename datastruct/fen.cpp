d1a844b0edc757ae5a26b1e99488b9a4f9d951c4
Henrik Aalto <hltk@hltk.fi>
2021-01-20 12:42:29 +0200

    add fenwick tree

---
+++ datastruct/fen.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+
+// source: kactl
+template <typename T>
+struct FenTree {
+	std::vector<T> tree;
+	FenTree(typename std::vector<T>::size_type n) : tree(n) {}
+	// a[i] += dif
+	void update(int i, T d) {
+		for (; i < int(tree.size()); i |= i + 1) tree[i] += d;
+	}
+	// sum of values in [0, r)
+	T query(int r) {
+		T ret = 0;
+		for (; r > 0; r &= r - 1) ret += tree[r - 1];
+		return ret;
+	}
+	// returns min pos such that sum of [0, pos] >= sum
+	// n if no sum is >= sum
+	// -1 if empty sum is.
+	int lower_bound(T sum) {
+		if (sum <= 0) return -1;
+		int pos = 0;
+		for (int pw = 1 << 25; pw; pw >>= 1) {
+			if (pos + pw <= int(tree.size()) && tree[pos + pw - 1] < sum) {
+				pos += pw, sum -= tree[pos - 1];
+			}
+		}
+		return pos;
+	}
+};
