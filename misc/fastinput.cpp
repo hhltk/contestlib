a032de367e0445764d391a908f65aef200933e89
Henrik Aalto <hltk@hltk.fi>
2021-01-20 12:04:14 +0200

    update fastinput.cpp

--- misc/fastinput.cpp
+++ misc/fastinput.cpp
@@ -1,34 +1,46 @@
 #include <cctype>
 #include <cstdio>
-using namespace std;
+#include <string>
+#include <type_traits>
 
-// FIXME: Implement FastInput & FastOutput and make
-// function the same way as std::cin and std::cout
-
-// source: kactl, own
-struct Reader {
+struct FastInput {
+	template <typename T>
+	std::enable_if_t<std::is_integral_v<T>> operator>>(T& t) {
+		char c;
+		for (c = next_char(); c != '-' && !isdigit(c); c = next_char());
+		bool neg = c == '-';
+		if (neg) c = next_char();
+		for (t = 0; isdigit(c); c = next_char()) {
+			t = t * 10 + c - '0';
+		}
+		if (neg) t = -t;
+	}
+	template <typename T>
+	std::enable_if_t<std::is_floating_point_v<T>> operator>>(T& t) {
+		std::string s;
+		this->operator>>(s);
+		t = static_cast<T>(std::stold(s));
+	}
+	void operator>>(std::string& s) {
+		s.clear();
+		char c;
+		while (isspace(c = next_char()));
+		do {
+			s.push_back(c);
+		} while (!isspace(c = next_char()));
+	}
+	void operator>>(char& c) {
+		while (isspace(c = next_char()));
+	}
+private:
 	char buf[1 << 20];
-	int it = 0, n = 0;
-	char operator()() {
+	size_t it = 0, n = 0;
+	char next_char() {
 		if (it >= n) {
 			buf[0] = 0;
 			it = 0;
-			n = fread(buf, 1, sizeof(buf), stdin);
+			n = std::fread(buf, 1, sizeof(buf), stdin);
 		}
 		return buf[it++];
 	}
-} reader;
-int next_int() {
-	char c = reader();
-	while (c != '-' && (isdigit(c) == 0)) {
-		c = reader();
-	}
-	if (c == '-') {
-		return -next_int();
-	}
-	int a = c - '0';
-	while (isdigit(c = reader()) != 0) {
-		a = a * 10 + c - '0';
-	}
-	return a;
-}
+};
