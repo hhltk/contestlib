32ef44ab25ddaa417150373a52b3dfaee1cae907
Henrik Aalto <hltk@hltk.fi>
2021-01-21 09:10:53 +0200

    fact update syntax

--- math/fact.cpp
+++ math/fact.cpp
@@ -4,37 +4,37 @@ using namespace std;
 #include "modint.cpp"
 
 namespace comb {
-vector<mint> fact, ifact;
+	vector<mint> fact, ifact;
 
-void ensure_facts(int n) {
-	if (int(fact.size()) >= n) {
-		return;
+	void ensure_facts(int n) {
+		if (int(fact.size()) >= n) {
+			return;
+		}
+		if (fact.empty()) {
+			fact.emplace_back(1);
+			ifact.emplace_back(1);
+		}
+		while (int(fact.size()) < n) {
+			fact.push_back(fact.back() * fact.size());
+			ifact.push_back(1 / fact.back());
+		}
 	}
-	if (fact.empty()) {
-		fact.emplace_back(1);
-		ifact.emplace_back(1);
+
+	mint choose(int a, int b) {
+		if (a < b || b < 0) {
+			return 0;
+		}
+		ensure_facts(a);
+		return fact[a] * ifact[b] * ifact[a - b];
 	}
-	while (int(fact.size()) < n) {
-		fact.push_back(fact.back() * fact.size());
-		ifact.push_back(1 / fact.back());
+
+	mint factorial(int a) {
+		ensure_facts(a);
+		return fact[a];
+	}
+
+	mint inv_factorial(int a) {
+		ensure_facts(a);
+		return ifact[a];
 	}
 }
-
-mint choose(int a, int b) {
-	if (a < b || b < 0) {
-		return 0;
-	}
-	ensure_facts(a);
-	return fact[a] * ifact[b] * ifact[a - b];
-}
-
-mint factorial(int a) {
-	ensure_facts(a);
-	return fact[a];
-}
-
-mint inv_factorial(int a) {
-	ensure_facts(a);
-	return ifact[a];
-}
-} // namespace comb
