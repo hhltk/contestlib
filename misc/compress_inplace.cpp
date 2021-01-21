9d281254dff168f463df29280b122c6b039f93db
Henrik Aalto <hltk@hltk.fi>
2021-01-21 08:59:54 +0200

    add compress inplace

---
+++ misc/compress_inplace.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+template<typename T>
+void compress(vector<T>& v) {
+	vector<pair<T, int>> p(v.size());
+	for (int i = 0; i < int(v.size()); ++i) p[i] = {v[i], i};
+	sort(p.begin(), p.end());
+	T pr = p[0].first;
+        int k = 0;
+	for (auto [x, i] : p) {
+		k += pr != x;
+		pr = x;
+		v[i] = k;
+	}
+}
+
+int main(){}
