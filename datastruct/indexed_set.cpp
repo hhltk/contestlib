91494fdef848f1ac767af92c2d6a38f7277dfa3f
Henrik Aalto <hltk@hltk.fi>
2021-01-21 20:38:58 +0200

    update indexed_set.cpp

--- datastruct/indexed_set.cpp
+++ datastruct/indexed_set.cpp
@@ -8,38 +8,28 @@
 
 namespace pbds {
 	const int INF = std::numeric_limits<int>::max();
-
 	using namespace __gnu_pbds;
 
-	template<class T, class C = std::less<T>>
-	using set = tree<T, null_type, C, rb_tree_tag, tree_order_statistics_node_update>;
-
-	// Use with pairs to get a multiset
-	// Useful functions:
-	// 	tree.order_of_key
-	// 	tree.find_by_order
+	template<class T, class C = std::less<std::pair<T, int>>>
+	using set = tree<std::pair<T, int>, null_type, C, rb_tree_tag, tree_order_statistics_node_update>;
 
 	template<class T>
 	int at_most(set<std::pair<T, int>>& s, T k) {
 		return s.order_of_key({k, INF});
 	}
-
 	template<class T>
 	int at_least(set<std::pair<T, int>>& s, T k) {
 		return s.size() - at_most(s, k - 1);
 	}
-
 	template<class T>
 	int range_sum(set<std::pair<T, int>>& s, T l, T r) {
 		return at_most(s, r) - at_most(s, l - 1);
 	}
-
 	template<class T>
 	void insert(set<std::pair<T, int>>& s, T x) {
 		static int t = 0;
 		s.insert(std::make_pair(x, t++));
 	}
-
 	template<class T>
 	void erase(set<std::pair<T, int>>& s, T x) {
 		s.erase(s.lower_bound(std::make_pair(x, -INF)));
