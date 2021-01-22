c9788f8cbce8e9bcd34f2b912a1ea6c2b2c5a09e
Henrik Aalto <hltk@hltk.fi>
2021-01-22 12:27:58 +0200

    update unionfind.cpp

---
+++ datastruct/dsu.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <numeric>
+
+class DSU {
+	std::vector<int> f, siz;
+public:
+	DSU(int n) : f(n), siz(n, 1) { std::iota(f.begin(), f.end(), 0); }
+	int root(int x) {
+		while (x != f[x]) x = f[x] = f[f[x]];
+		return x;
+	}
+	bool same(int x, int y) { return root(x) == root(y); }
+	bool merge(int x, int y) {
+		x = root(x);
+		y = root(y);
+		if (x == y) return false;
+		siz[x] += siz[y];
+		f[y] = x;
+		return true;
+	}
+	int size(int x) { return siz[root(x)]; }
+};
