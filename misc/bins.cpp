d133b8672c13f1d800e33665f2d4606b9adc89c4
Henrik Aalto <hltk@hltk.fi>
2021-03-27 19:46:11 +0200

    bins.cpp: fix compiler warning

--- misc/bins.cpp
+++ misc/bins.cpp
@@ -2,7 +2,7 @@
 // i.e. minimum x such that f(x) = true and x \in [l, r)
 auto bins = [](auto l, auto r, auto f) {
 	while (l < r) {
-		int m = l + (r - l) / 2;
+		auto m = l + (r - l) / 2;
 		if (f(m)) r = m;
 		else l = m + 1;
 	}
